bab6b8bd58abf3dff3a109ebc4968ba7e0662e2f
Mohammod Ali <mohammodali651@gmail.com>
2024-10-23 03:11:30 +0600

    section title dynamically added

--- src/Page/Home/Category/Category.jsx
+++ src/Page/Home/Category/Category.jsx
@@ -9,14 +9,21 @@ import img2 from '../../../assets/home/slide2.jpg'
 import img3 from '../../../assets/home/slide3.jpg'
 import img4 from '../../../assets/home/slide4.jpg'
 import img5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
 
 
 
 const Category = () => {
     return (
-        <Swiper
+        <section>
+            <SectionTitle 
+            heading={"Order Online"}
+            subHeading={"From 11am to 10pm"}
+            ></SectionTitle>
+            <Swiper
         slidesPerView={3}
         spaceBetween={30}
+        centeredSlides={true}
         pagination={{
           clickable: true,
         }}
@@ -45,6 +52,7 @@ const Category = () => {
         </SwiperSlide>
         
       </Swiper>
+        </section>
     );
 };
 
