b1aa0e4d81f8d3b10881cc150bcacda428b0344f
Mohammod Ali <mohammodali651@gmail.com>
2024-10-29 02:56:16 +0600

    parallax added

--- src/Page/Menu/Menu/Menu.jsx
+++ src/Page/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet,  } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 
 const Menu = () => {
@@ -7,7 +10,8 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h1>mememememememme</h1>
+            <Cover img={menuImg} title='our menu' description={'Would you like to try a dish?'}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
