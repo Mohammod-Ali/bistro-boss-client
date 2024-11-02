afec6e40a1c31f3162b7cf73363d425ce00ba45d
Mohammod Ali <mohammodali651@gmail.com>
2024-11-02 23:39:22 +0600

    data pass dynamically from backend

--- src/Page/Shared/FoodCart/FoodCart.jsx
+++ src/Page/Shared/FoodCart/FoodCart.jsx
@@ -10,11 +10,11 @@ const FoodCart = ({item}) => {
       alt="Shoes" />
   </figure>
   <p className=" absolute right-0 mt-4 mr-4 px-3 bg-slate-900 text-white">${price}</p>
-  <div className="card-body">
+  <div className="card-body flex flex-col items-center">
     <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Add to Cart</button>
+      <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">Add to Cart</button>
     </div>
   </div>
 </div>
