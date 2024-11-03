7d3c8a82828b6929d8498f099224bfc8aae2fb82
Mohammod Ali <mohammodali651@gmail.com>
2024-11-04 00:59:53 +0600

    swiper pagination added

--- src/Page/Order/OrderTab/OrderTab.jsx
+++ src/Page/Order/OrderTab/OrderTab.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import FoodCart from '../../Shared/FoodCart/FoodCart';
+import React from "react";
+import FoodCart from "../../Shared/FoodCart/FoodCart";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper/modules";
 
-const OrderTab = ({items}) => {
-    return (
+const OrderTab = ({ items }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+  return (
+    <Swiper pagination={pagination} modules={[Pagination]} className="mySwiper">
+      <SwiperSlide>
         <div className="grid md:grid-cols-3 gap-10">
-        {
-              items.map(item => <FoodCart
-              key={item._id}
-              item={item}
-              ></FoodCart>)
-          }
+          {items.map((item) => (
+            <FoodCart key={item._id} item={item}></FoodCart>
+          ))}
         </div>
-    );
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default OrderTab;
