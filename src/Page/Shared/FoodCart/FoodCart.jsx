e4529483c0ec63545b263bcfc30a5e8206abc364
Mohammod Ali <mohammodali651@gmail.com>
2024-11-14 15:47:00 +0600

    useAuth hook and food cart data send in backend funcionality added

--- src/Page/Shared/FoodCart/FoodCart.jsx
+++ src/Page/Shared/FoodCart/FoodCart.jsx
@@ -1,9 +1,34 @@
+import Swal from "sweetalert2";
+import useAuth from "../../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+
 
 const FoodCart = ({item}) => {
     const {image, name, price, recipe} = item
+    const {user} = useAuth()
+    const navigate = useNavigate()
 
     const handleAddToCart = food => {
-      console.log(food)
+      if(user && user.email){
+
+      }else{
+        Swal.fire({
+          title: "You are not Logged In",
+          text: "Please login to add to the cart",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, login"
+        }).then((result) => {
+          if (result.isConfirmed) {
+           // send the user to the login page
+           navigate('/login')
+          }
+        });
+        
+      }
     }
 
     return (
