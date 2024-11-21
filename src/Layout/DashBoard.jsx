840b657aa1a674eb462edca010b1f5872dbf8692
Mohammod Ali <mohammodali651@gmail.com>
2024-11-21 23:54:03 +0600

    dashboard list added

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from 'react-icons/fa';
 import { NavLink, Outlet } from 'react-router-dom';
 
 const DashBoard = () => {
     return (
-        <div>
+        <div className='flex'>
             <div className='w-64 min-h-full bg-orange-400'>
                 <ul className='menu'>
-                    <li><NavLink to='/dashboard/cart'>My Cart</NavLink></li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/userHome'><FaHome></FaHome> User Home</NavLink>
+                    </li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/reservation'><FaCalendar></FaCalendar> Reservation</NavLink>
+                    </li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart</NavLink>
+                    </li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/review'><FaAd></FaAd> Review</NavLink>
+                    </li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/bookings'><FaList></FaList> My Bookings</NavLink>
+                    </li>
                 </ul>
             </div>
+            {/* dashboard layout */}
             <div className='flex-1'>
                 <Outlet></Outlet>
             </div>
