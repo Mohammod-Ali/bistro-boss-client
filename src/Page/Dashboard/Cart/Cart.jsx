524fc53d048d0fec11ed20e41b9e22d15c5ba4f4
Mohammod Ali <mohammodali651@gmail.com>
2024-11-24 22:52:38 +0600

    delete alart added

--- src/Page/Dashboard/Cart/Cart.jsx
+++ src/Page/Dashboard/Cart/Cart.jsx
@@ -1,8 +1,31 @@
+import { FaTrash } from "react-icons/fa";
 import useCart from "../../../hooks/useCart";
 
 const Cart = () => {
   const [cart] = useCart();
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  const handleDelete = id => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+  }
+
+
   return (
     <div>
       <div className="flex justify-evenly mb-8">
@@ -48,7 +71,9 @@ const Cart = () => {
                   </td>
                   <td>$ {item.price}</td>
                   <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
+                    <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-lg">
+                      <FaTrash className="text-red-500"></FaTrash>
+                    </button>
                   </th>
                 </tr>)
             }
