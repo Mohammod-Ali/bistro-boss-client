d4dcf7f9e20cd58e8f174f4c175fbdb6e3d9946d
Mohammod Ali <mohammodali651@gmail.com>
2024-12-14 19:31:59 +0600

    select category items added

--- src/Page/Dashboard/AddItems/AddItems.jsx
+++ src/Page/Dashboard/AddItems/AddItems.jsx
@@ -1,12 +1,38 @@
+import { useForm } from "react-hook-form";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
-
 const AddItems = () => {
-    return (
-        <div>
-            <SectionTitle heading='add an item' subHeading="What's new?"></SectionTitle>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <SectionTitle
+        heading="add an item"
+        subHeading="What's new?"
+      ></SectionTitle>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name")} />
+          <select
+            {...register("category")}
+            className="select select-bordered w-full max-w-xs"
+          >
+            <option disabled selected>
+              Select a Category
+            </option>
+            <option value="salad">Salad</option>
+            <option value="pizza">Pizza</option>
+            <option value="soup">Soup</option>
+            <option value="dessert">Dessert</option>
+            <option value="drinks">Drinks</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItems;
